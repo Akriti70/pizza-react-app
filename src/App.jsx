e531c55153c703dd972ff114180c59c4957c5912
Oleksandr Bakai <saniabakay@gmail.com>
2025-09-20 10:59:37 +0200

    send supabase data to Menu,fetchData is in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useEffect, useState } from "react"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import supabasePizzas from "./supabase/SupabasePizzas"
+import Menu from "./pages/Menu"
 
 export default function App() {
+  //data for Pizzas
+  const [pizzas,setPizzas]=useState([])
+  useEffect(()=>{
+    async function fetchData() {
+      const data = await supabasePizzas()
+      setPizzas(data)
+      }
+      fetchData()
+  },[])
+  ///
+
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
@@ -20,7 +34,7 @@ export default function App() {
             <Route path="/signup" element={<Signup />} />
             
             {/* Partner ke liye future routes */}
-            <Route path="/menu" element={<h1 className="p-10 text-3xl">Menu Page 🍕</h1>} />
+            <Route path="/menu" element={<Menu pizzas={pizzas} className="p-10 text-3xl">Menu Page 🍕</Menu>} />
             <Route path="/cart" element={<h1 className="p-10 text-3xl">Cart Page 🛒</h1>} />
             <Route path="/checkout" element={<h1 className="p-10 text-3xl">Checkout Page ✅</h1>} />
           </Routes>
--- src/supabase/SupabasePizzas.js
+++ src/supabase/SupabasePizzas.js
@@ -1,6 +1,6 @@
 import { supabase } from '../supabase/supabaseClient'
 
-export async function supabasePizzas() {
+export default async function supabasePizzas() {
   const { data, error } = await supabase.from('pizzas').select('*');
   if (error) {
     console.error('Error fetching pizzas:', error);
