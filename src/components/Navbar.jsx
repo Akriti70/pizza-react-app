4962113e156f9a9ae1ac5b9f2a57980d09be54cf
Oleksandr Bakai <saniabakay@gmail.com>
2025-09-20 17:43:25 +0200

    in Navbar added NavLink for Menu, when menu is active it will be highlighted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,28 @@
-
-import { Link } from "react-router-dom"
-import "./Navbar.css"
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+import {NavLink} from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <Link to="/" className="logo"> PizzaApp</Link>
+        <Link to="/" className="logo">
+          {' '}
+          PizzaApp
+        </Link>
         <div className="nav-links">
           <Link to="/">Home</Link>
-          <Link to="/menu">Menu</Link>
+          <NavLink
+            to="/menu"
+            className={({isActive}) => (isActive ? 'active-link' : '')}
+          >
+            Menu
+          </NavLink>
           <Link to="/cart">Cart</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
         </div>
       </div>
     </nav>
-  )
+  );
 }
